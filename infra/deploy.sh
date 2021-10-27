04d1e2876dcac67e34069822a0dfdafae55ecc45
Sruinard <41334908+Sruinard@users.noreply.github.com>
2021-10-27 05:39:31 +0000

    deploy infra

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,5 +5,5 @@ az group create --name prod-tripplanner --location westeurope
 az group create --name dev-tripplanner --location westeurope
 
 # CHECK: 'uniqueness' - make this unique for your local
-az deployment group create --resource-group prod-tripplanner --template-file './infra/apiworkload.bicep' --parameters uniqueness=sruinard --environment=prod --mode Complete
-az deployment group create --resource-group dev-tripplanner --template-file './infra/apiworkload.bicep' --parameters uniqueness=sruinard --environment=dev --mode Complete
+az deployment group create --resource-group prod-tripplanner --template-file './infra/apiworkload.bicep' --parameters uniqueness=sruinard environment=prod --mode Complete
+az deployment group create --resource-group dev-tripplanner --template-file './infra/apiworkload.bicep' --parameters uniqueness=sruinard environment=dev --mode Complete
